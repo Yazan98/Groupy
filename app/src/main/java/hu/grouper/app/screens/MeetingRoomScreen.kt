f47bb188268f0cd09435f6a7c8324acd5fdeaf62
Yazan <yazantarifi98@gmail.com>
2019-12-14 14:03:25 +0300

    finish meeting screen

--- app/src/main/java/hu/grouper/app/screens/MeetingRoomScreen.kt
+++ app/src/main/java/hu/grouper/app/screens/MeetingRoomScreen.kt
@@ -1,14 +1,18 @@
 package hu.grouper.app.screens
 
-import android.location.Location
 import android.os.Bundle
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import hu.grouper.app.R
 import hu.grouper.app.adapter.LocationAdapter
@@ -16,13 +20,7 @@ import hu.grouper.app.data.models.Profile
 import io.vortex.android.prefs.VortexPrefs
 import io.vortex.android.ui.activity.VortexScreen
 import kotlinx.android.synthetic.main.screen_meeting_room.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.MarkerOptions
-
+import kotlinx.coroutines.*
 
 
 /**
@@ -32,7 +30,7 @@ import com.google.android.gms.maps.model.MarkerOptions
  */
 
 class MeetingRoomScreen : VortexScreen(), GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
+    GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var mGoogleApiClient: GoogleApiClient
@@ -40,7 +38,14 @@ class MeetingRoomScreen : VortexScreen(), GoogleApiClient.ConnectionCallbacks,
     private val mainAdapter: LocationAdapter by lazy {
         LocationAdapter(object : LocationAdapter.LocationListener {
             override fun onCLick(lat: Double?, lng: Double?) {
-
+                mMap.moveCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(
+                            lat!!,
+                            lng!!
+                        ), 15f
+                    )
+                )
             }
         })
     }
@@ -56,29 +61,47 @@ class MeetingRoomScreen : VortexScreen(), GoogleApiClient.ConnectionCallbacks,
 
         GlobalScope.launch {
             FirebaseFirestore.getInstance().collection("groups")
-                    .document(VortexPrefs.get("GroupID", "") as String)
-                    .get().addOnCompleteListener {
-                        it.result?.let {
+                .document(VortexPrefs.get("GroupID", "") as String)
+                .get().addOnCompleteListener {
+                    it.result?.let {
+                        GlobalScope.launch {
                             for (member in it.get("members") as List<String>) {
                                 FirebaseFirestore.getInstance().collection("users").document(member)
-                                        .get().addOnCompleteListener {
-                                            it.result?.let {
-                                                mainAdapter.add(Profile(
-                                                        id = it.getString("id"),
-                                                        name = it.getString("name"),
-                                                        lat = it.getDouble("lat"),
-                                                        lng = it.getDouble("lng")
-                                                ))
-
-                                                GlobalScope.launch {
-                                                    addCurrentLocationMarker(LatLng(it.getDouble("lat")!!
-                                                            , it.getDouble("lng")!!) , it.getString("name")!!)
-                                                }
-                                            }
+                                    .get().addOnCompleteListener {
+                                        it.result?.let {
+                                            mainAdapter.add(
+                                                Profile(
+                                                    id = it.getString("id"),
+                                                    name = it.getString("name"),
+                                                    lat = it.getDouble("lat"),
+                                                    lng = it.getDouble("lng")
+                                                )
+                                            )
                                         }
+                                    }
+                            }
+
+                            delay(2000)
+                            for (item in mainAdapter.items) {
+                                GlobalScope.launch {
+                                    println("Item : $item")
+                                    addCurrentLocationMarker(LatLng(item.lat!!, item.lng!!), item.name!!)
+                                }
+                            }
+
+                            withContext(Dispatchers.Main) {
+                                mMap.moveCamera(
+                                    CameraUpdateFactory.newLatLngZoom(
+                                        LatLng(
+                                            mainAdapter.items[0].lat!!,
+                                            mainAdapter.items[0].lng!!
+                                        ), 15f
+                                    )
+                                )
                             }
                         }
                     }
+                }
         }
 
         MeetingsRecycler?.apply {
@@ -91,10 +114,10 @@ class MeetingRoomScreen : VortexScreen(), GoogleApiClient.ConnectionCallbacks,
 
     private fun buildGoogleApiClient() {
         mGoogleApiClient = GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build()
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
     }
 
     override fun onConnectionFailed(@NonNull connectionResult: ConnectionResult) = Unit
@@ -104,14 +127,13 @@ class MeetingRoomScreen : VortexScreen(), GoogleApiClient.ConnectionCallbacks,
         mGoogleApiClient.connect()
     }
 
-    private suspend fun addCurrentLocationMarker(location: LatLng , nane:String) {
+    private suspend fun addCurrentLocationMarker(location: LatLng, nane: String) {
         println("Location Screen : $location")
         withContext(Dispatchers.Main) {
             val markerOptions = MarkerOptions()
             markerOptions.position(location)
-            mMap.clear()
             markerOptions.title(nane)
-            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.place))
+//            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.place))
             markerOptions.position
             mMap.addMarker(markerOptions)
         }
